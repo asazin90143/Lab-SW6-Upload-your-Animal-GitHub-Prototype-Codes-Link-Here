983c64297d8ffeb0225cca732a20bcdbef4c4e23
KENNETH BRYAN GERABAS ESCALA <69161486+asazin90143@users.noreply.github.com>
2026-04-07 03:02:50 +0000

    corection

--- Cow.java
+++ Cow.java
@@ -1,6 +1,26 @@
 public class Cow implements Animal {
-    private Integer legs;
+    private int legs;
     private String sound;
     private String food;
-    private String name;
+
+    public Cow(int legs, String sound, String food) {
+        this.legs = legs;
+        this.sound = sound;
+        this.food = food;
+    }
+
+    @Override
+    public Animal clone() {
+        return new Cow(legs, sound, food);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Cow says: " + sound);
+    }
+
+    @Override
+    public String getType() {
+        return "Cow";
+    }
 }
